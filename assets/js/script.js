4d0bd979fb405bd0f94cf2c252f2c80f1aca9f82
Arafat <placeholderause4590@gmail.com>
2025-09-17 10:49:00 +0600

    uplode project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,109 @@
 $(document).ready(function() {
 
-  // body-darkmood
+  // preloder-start
 
-  $(document).ready(function() {
-    $('#dark-mode-toggle').on('click', function() {
-      $('body').toggleClass('dark-mode');
+  $(window).on('load', function() {
+    $('#preloader').delay(1000).fadeOut(5000, function() {
+      $(this).remove(); 
     });
   });
 
-    // Open responsive slide detailss
-    $('.side-details-button').on('click', function() {
-      $('.side-details').toggleClass("left-0");
-    });
 
-    // Open responsive slide detailss
-    $('.side-icons-list-button').on('click', function() {
-      $('.side-icons-list').toggleClass("right-0").removeClass("hidden").addClass("flex");
-    });
+  // aos animation
 
+  AOS.init({
+    duration: 1000,
   });
 
-  $('.project-count').counterUp({
-    delay: 10,
-    time: 2000
-  });
+  // navbar
+    $(".navigation-trigger").click(function () {
+      $(".responsive-navbar, .overlay").removeClass("-left-full").addClass("left-0");
+    });
 
+    // Close navbar (overlay or X button click)
+    $(".overlay, .close-nav").click(function () {
+      $(".responsive-navbar, .overlay").removeClass("left-0").addClass("-left-full");
+    });
+  // navigation-active
 
+  $(".navigation-bar li a").click(function(){
+    $(".navigation-bar li a").removeClass("active");
+    $(this).addClass("active");
+  })
 
-  $('.recommendations-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    dots: true,
-    dotsEach: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
+// Alternative - শুধু autoplay direction change করুন, carousel destroy করবেন না
+let isRTL = true;
+let owl;
+
+function initCarousel() {
+    owl = $('.brand-carousel').owlCarousel({
+        rtl: isRTL,
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3 },
+            1200: { items: 4 },
+            1500: { items: 5 },
+            1600: { items: 6 },
         }
+    });
+}
+
+// Initial carousel
+initCarousel();
+
+// শুধু autoplay direction change করুন
+setInterval(function() {
+    // Stop current autoplay
+    owl.trigger('stop.owl.autoplay');
+    
+    // Toggle direction flag
+    isRTL = !isRTL;
+    
+    // Manually control slide direction
+    if (isRTL) {
+        // Right to left movement
+        owl.trigger('prev.owl.carousel');
+    } else {
+        // Left to right movement  
+        owl.trigger('next.owl.carousel');
     }
-})
+    
+    // Resume autoplay
+    setTimeout(function() {
+        owl.trigger('play.owl.autoplay');
+    }, 100);
+    
+    console.log('Direction:', isRTL ? 'Right to Left' : 'Left to Right');
+    
+}, 2000);
 
-
-$('.gallery').magnificPopup({
-  delegate: '.gllary-image',
-  type: 'image',
-  gallery: {
-    enabled: true
-  }
 });
 
-// Initialize Isotope
-  // Initialize isotope
-  $(document).ready(function() {
-    // init Isotope
-    var $grid = $('.gallery').isotope({
-      itemSelector: ".gllary-image",
-      layoutMode: 'fitRows' // এটি গ্রিড লেআউট ভাঙবে না
-    });
-  
-    // filter items on button click
-    $('.gllary-filter').on('click', 'li', function() {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
-    
-      $('.gllary-filter li').removeClass('active');
-      $(this).addClass('active');
-    });
-  });
-// active-link
+// number-count
 
-$(".gllary-filter li").click(function(){
-  $(this).removeClass("active")
-})
+$('.number-counter').counterUp({
+  delay: 10,
+  time: 1000
+});
 
-$(".gllary-filter li").click(function(){
-  $(".gllary-filter li").addClass("active")
+$('.testiomonial-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  dots: false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+  }
 })
